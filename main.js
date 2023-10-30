7b3621f3c35f89c918c8b372694be6d1262b8030
Gianfranco Mamone <gianfrancomamone20@gmail.com>
2023-10-30 19:12:09 -0300

    arreglos

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let PIN = "42325";
 let ingresar = false;
 let usuarios = [
+  {nombre:"usuario",PIN:"42325",saldo:50000},
   { nombre: "Juan", PIN: "12345", saldo: 50000 },
   { nombre: "María", PIN: "54321", saldo: 35000 },
   { nombre: "Pedro", PIN: "67890", saldo: 70000 }
@@ -10,7 +11,7 @@ function ingreso() {
   alert("Bienvenidos a Banco Columbia");
   for (let i = 2; i >= 0; i--) {
     let ingresoPIN = prompt("Ingresa tu PIN");
-    if (ingresoPIN == PIN) {
+    if (ingresoPIN === PIN) {
       alert("Disfrute su Estadia");
       ingresar = true;
       break;
@@ -23,6 +24,8 @@ function ingreso() {
 ingreso();
 
 if (ingresar) {
+  let usuario;
+
   let opcion = prompt(
     "Elige una opción:\n1- Consultar saldo.\n2- Retirar dinero.\n3- Depositar dinero.\nPresiona 'x' para finalizar."
   );
@@ -30,23 +33,23 @@ if (ingresar) {
   while (opcion !== "x") {
     switch (opcion) {
       case "1":
-        let usuario = usuarios.find((u) => u.PIN === PIN);
+        usuario = usuarios.find((u) => u.PIN === PIN);
         alert(`Tu saldo es: ${usuario.saldo}`);
         break;
       case "2":
+        usuario = usuarios.find((u) => u.PIN === PIN);
         let retiro = parseFloat(prompt("Ingrese el monto que desea retirar."));
-        let usuarioRetiro = usuarios.find((u) => u.PIN === PIN);
-        if (retiro <= usuarioRetiro.saldo) {
-          usuarioRetiro.saldo -= retiro;
+        if (retiro <= usuario.saldo) {
+          usuario.saldo -= retiro;
           alert("Retirado con éxito");
         } else {
           alert("Saldo insuficiente");
         }
         break;
       case "3":
+        usuario = usuarios.find((u) => u.PIN === PIN);
         let deposito = parseFloat(prompt("Ingrese el monto que desea depositar."));
-        let usuarioDeposito = usuarios.find((u) => u.PIN === PIN);
-        usuarioDeposito.saldo += deposito;
+        usuario.saldo += deposito;
         alert("Depósito exitoso");
         break;
       default:
